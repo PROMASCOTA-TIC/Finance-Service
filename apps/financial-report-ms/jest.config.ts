5704ea20837a9c2ded8e1125f7af0635a2966e82
KevinCondor <kevin.condor@epn.edu.ec>
2024-11-22 11:02:27 -0500

    first commit

---
+++ apps/financial-report-ms/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'financial-report-ms',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/financial-report-ms',
+};
