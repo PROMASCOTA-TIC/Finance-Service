5ab573f1155d3e7e52cf6188066bfe5ad337ca6b
KevinCondor <kevin.condor@epn.edu.ec>
2024-12-04 23:05:07 -0500

    feat: Actualizar modulo de reporte financiero para usar NATS como canal de comunicación y eliminación de DTOs que no se ocupan

--- apps/financial-report-ms/src/app/financial-report/dto/update-financial-report.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateFinancialReportDto } from './create-financial-report.dto';
-
-export class UpdateFinancialReportDto extends PartialType(CreateFinancialReportDto) {}
--- apps/financial-report-ms/src/app/financial-report/financial-report.controller.ts
+++ apps/financial-report-ms/src/app/financial-report/financial-report.controller.ts
@@ -1,14 +1,14 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { FinancialReportService } from './financial-report.service';
 import { DateRangeDto } from './dto/date-range.dto';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller('financial-report')
 export class FinancialReportController {
   constructor(private readonly financialReportService: FinancialReportService) {}
 
-  @Get('data')
-  getFinanceData(@Body() dateRange: DateRangeDto) {
-    console.log('Getting finance data');
+  @MessagePattern('get_finance_data')
+  getFinanceData(@Payload() dateRange: DateRangeDto) {
     return this.financialReportService.getFinanceData(dateRange);
   }
 }
