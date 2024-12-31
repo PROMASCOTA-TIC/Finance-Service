9aada7b180fe630144c70a11c8f00b5d5b3ad808
KevinCondor <kevin.condor@epn.edu.ec>
2024-12-30 23:28:43 -0500

    feat: Implementar métodos para obtener ventas y mejorar el manejo de datos financieros

--- apps/financial-report-ms/src/app/financial-report/financial-report.service.ts
+++ apps/financial-report-ms/src/app/financial-report/financial-report.service.ts
@@ -1,12 +1,39 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Search } from '@nestjs/common';
 import { firstValueFrom } from 'rxjs';
 import { GetByDateRangeDto } from './dto/get-by-date-range.dto';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class FinancialReportService {
 
   constructor(private httpService: HttpService) { }
+
+  async getSalesByRangeDate(dateRange: GetByDateRangeDto) {
+    let startDate = dateRange.startDate;
+    let endDate = dateRange.endDate;
+    const currentYear = new Date().getFullYear();
+
+    if (!startDate) {
+      startDate = new Date(currentYear, 0, 1, 0, 0, 0);
+    }
+
+    if (!endDate) {
+      endDate = new Date(currentYear, 11, 31, 23, 59, 59);
+    }
+    try{
+      const sales = await firstValueFrom(
+        this.httpService.post('http://localhost:3001/api/incomes/sales-date-range', {
+          startDate: startDate,
+          endDate: endDate
+        })
+      )
+    return sales.data;
+    }
+    catch(error){
+      throw new RpcException(error);
+    }
+  }
 
   async getIncomesByRangeDate(dateRange: GetByDateRangeDto) {
     let startDate = dateRange.startDate;
@@ -22,7 +49,7 @@ export class FinancialReportService {
     }
 
     const incomes = await firstValueFrom(
-      this.httpService.post('http://localhost:3000/api/incomes/range', {
+      this.httpService.post('http://localhost:3001/api/incomes/date-range', {
         startDate: startDate,
         endDate: endDate
       })
@@ -44,7 +71,7 @@ export class FinancialReportService {
     }
 
     const expenses = await firstValueFrom(
-      this.httpService.post('http://localhost:3000/api/expenses/range', {
+      this.httpService.post('http://localhost:3001/api/expenses/range', {
         startDate: startDate,
         endDate: endDate
       })
@@ -52,27 +79,76 @@ export class FinancialReportService {
     return expenses.data;
   }
 
+  async getEntrepreneurName(entrepreneurId: string) {
+    const entrepreneur = await firstValueFrom(
+      this.httpService.get('http://localhost:3001/api/entrepreneur/' + entrepreneurId)
+    )
+    return entrepreneur.data;
+  }
+
+  async getProductsName(productId: string) {
+    const product = await firstValueFrom(
+      this.httpService.get('http://localhost:3001/api/products/' + productId)
+    )
+    return product.data;
+  }
+
   async getFinanceData(dateRange: GetByDateRangeDto) {
     const incomes = await this.getIncomesByRangeDate(dateRange);
     const expenses = await this.getExpensesByRangeDate(dateRange);
-    const incomesTotal = incomes
+    const sales = await this.getSalesByRangeDate(dateRange);
+    const incomesTotal = await incomes
       .map((income: any) => Number(income.price))
-      .reduce((a: number, b: number, index: number) => {
+      .reduce((a: number, b: number) => {
         const sum = a + b;
         return sum;
       }, 0);
 
-    const expensesTotal = expenses
+    const expensesTotal = await expenses
       .map((expense: any) => Number(expense.price))
-      .reduce((a: number, b: number, index: number) => {
+      .reduce((a: number, b: number) => {
         const sum = a + b;
         return sum;
       }, 0);
     const balance = parseFloat((incomesTotal - expensesTotal).toFixed(2));
 
+    const incomesData = await Promise.all(incomes.map(async (income: any) => {
+      // const entrepreneur = await this.getEntrepreneurName(income.entrepreneurId);
+      return income = {
+        id: income.id,
+        userName: '', //entrepreneur.name,
+        amount: Number(income.price),
+        incomeDate: new Date(income.createdAt)
+      };
+    }));
+
+    const expensesData = await Promise.all(expenses.map(async (expense: any) => {
+      return expense = {
+        id: expense.id,
+        category: expense.category,
+        description: expense.description,
+        price: Number(expense.price),
+        expenseDate: new Date(expense.expenseDate)
+      };
+    }));
+
+    const salesData = await Promise.all(sales.map(async (sale: any) => {
+      // const entrepreneur = await this.getEntrepreneurName(sale.entrepreneurId);
+      // const product = await this.getProductsName(sale.productId);
+      return sale = {
+        id: sale.id,
+        entrepreneurName:'', //entrepreneur.name,
+        productName: '', // poner el nombre que se traiga product.name,
+        productCategory: sale.productCategory, // poner la categoria que se traiga product.category.name,
+        amount: Number(sale.amount),
+        saleDate: new Date(sale.salesDate)
+      };
+    }));
+    console.log(salesData);
     const data = {
-      ingresos: incomes,
-      egresos: expenses,
+      ingresos: incomesData,
+      egresos: expensesData,
+      ventas: salesData,
       incomes: incomesTotal.toFixed(2),
       expenses: expensesTotal.toFixed(2),
       balance: balance.toFixed(2)
@@ -105,8 +181,8 @@ export class FinancialReportService {
 
       const allDays = ['lun', 'mar', 'mié', 'jue', 'vie', 'sáb', 'dom'];
       const result = allDays.map(day => ({
-        day,
-        total: dailyTotals[day] ? dailyTotals[day].toFixed(2) : '0.00'
+        label: day,
+        value: dailyTotals[day] ? dailyTotals[day].toFixed(2) : '0.00'
       }));
       return result;
     } else {
@@ -129,8 +205,8 @@ export class FinancialReportService {
 
       const allMonths = ['ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic'];
       const result = allMonths.map(month => ({
-        month,
-        total: monthlyTotals[month] ? monthlyTotals[month].toFixed(2) : '0.00'
+        label: month,
+        value: monthlyTotals[month] ? monthlyTotals[month].toFixed(2) : '0.00'
       }));
       return result;
     }
@@ -152,10 +228,10 @@ export class FinancialReportService {
     const incomes = await this.getIncomesByRangeDate(getByRangeDto);
     const totalIncome = incomes.reduce((acc: number, income: any) => acc + Number(income.price), 0);
     const data = incomes.reduce((acc: any, income: any) => {
-      if (!acc[income.productCategory]) {
-        acc[income.productCategory] = 0;
+      if (!acc[income.category]) {
+        acc[income.category] = 0;
       }
-      acc[income.productCategory] += Number(income.price);
+      acc[income.category] += Number(income.price);
       return acc;
     }, {});
 
@@ -190,12 +266,10 @@ export class FinancialReportService {
 
     const incomesTotal = incomes
       .map((income: any) => Number(income.price))
-      .reduce((a: number, b: number, index: number) => {
+      .reduce((a: number, b: number) => {
         const sum = a + b;
-        console.log(`Step ${index + 1}: ${a} + ${b} = ${sum}`);
         return sum;
       }, 0);
-
     return incomesTotal.toFixed(2);
   }
 
@@ -204,14 +278,10 @@ export class FinancialReportService {
 
     const expensesTotal = expenses
       .map((expense: any) => Number(expense.price))
-      .reduce((a: number, b: number, index: number) => {
+      .reduce((a: number, b: number) => {
         const sum = a + b;
-        console.log(
-          `Step ${index + 1}: ${a} + ${b} = ${sum}`
-        );
         return sum;
       }, 0);
-    
     return expensesTotal.toFixed(2);
   }
 
